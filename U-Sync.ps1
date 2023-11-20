d3f5bbec39aa3059e54048f788f95b3a0aaaca28
stefano-titotto <stefano.titotto@gmail.com>
2023-11-20 16:52:38 +0100

    aggiornamento parm default

--- U-Sync.ps1
+++ U-Sync.ps1
@@ -1,6 +1,6 @@
 # Set the source and destination directories
 $sourceDirectory  = (Get-Location).Path
-$destinationDirectory = "U:\DAG\STEFANO\Simulux"
+$destinationDirectory = "U:\DAG\STEFANO\"
 
 # Use Copy-Item to sync the directories
 Copy-Item -Path $sourceDirectory -Destination $destinationDirectory -Recurse -Force 
